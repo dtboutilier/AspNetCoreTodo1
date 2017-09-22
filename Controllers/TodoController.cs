992be7def2bb55f0a64e2a86f908d5e4d57b4dff
David Boutilier <dtboutilier@gmail.com>
2017-09-22 09:03:27 -0500

    First draft

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreTodo.Models;
+using AspNetCoreTodo.Services;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreTodo.Controllers
+{
+    public class TodoController : Controller
+    {
+        private readonly ITodoItemService _todoItemService;
+
+        public TodoController(ITodoItemService todoItemService)
+        {
+            _todoItemService = todoItemService;
+        }
+        // GET: /<controller>/
+        public async Task<IActionResult> IndexAsync()
+        {
+            // Get to-do items from database
+            var todoItems = await _todoItemService.GetIncompleteItemsAsync();
+
+            var model = new TodoViewModel()
+            {
+                Items = todoItems,
+            };
+
+            // Put items into a model
+
+            // Pass the view to a model and render
+            return View();
+        }
+    }
+}
